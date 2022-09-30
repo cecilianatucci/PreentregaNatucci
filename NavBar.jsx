6e26ce6e15182825914c4348e779f46e32795fe9
cecilianatucci <105084624+cecilianatucci@users.noreply.github.com>
2022-09-29 23:26:37 -0300

    Add files via upload

---
+++ NavBar.jsx
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+import "./NavBar.css";
+import ImagenLogo from "../../assets/logo.jpg";
+export const NavBar = () =>{
+    
+
+    return(
+        <nav className='navegation'>
+            <img className='logo' src={ImagenLogo} alt='logo'/>
+            <ul className='list'>
+            <li><a href='/'> inicio</a></li>
+            <li><a href='/'> camisas</a></li>
+            <li><a href='/'> zapatos</a></li>
+            <li><a href='/'> sombreros</a></li>
+        </ul>
+        </nav>
+    )
+}
+export default NavBar;
